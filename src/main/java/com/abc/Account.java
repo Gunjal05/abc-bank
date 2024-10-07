6fa26d264a02f1c8644c516589640d9cbcef0fcf
Gunjal05 <gunjaljain1997@gmail.com>
2024-10-07 11:46:44 +0530

    Update Account.java

--- src/main/java/com/abc/Account.java
+++ src/main/java/com/abc/Account.java
@@ -8,6 +8,7 @@ public class Account {
     public static final int CHECKING = 0;
     public static final int SAVINGS = 1;
     public static final int MAXI_SAVINGS = 2;
+    public static final int SUPER_SAVINGS = 3;
 
     private final int accountType;
     public List<Transaction> transactions;
@@ -35,23 +36,26 @@ public void withdraw(double amount) {
 
     public double interestEarned() {
         double amount = sumTransactions();
-        switch(accountType){
-            case SAVINGS:
-                if (amount <= 1000)
-                    return amount * 0.001;
-                else
-                    return 1 + (amount-1000) * 0.002;
-//            case SUPER_SAVINGS:
-//                if (amount <= 4000)
-//                    return 20;
-            case MAXI_SAVINGS:
-                if (amount <= 1000)
-                    return amount * 0.02;
-                if (amount <= 2000)
-                    return 20 + (amount-1000) * 0.05;
-                return 70 + (amount-2000) * 0.1;
-            default:
-                return amount * 0.001;
+        switch (accountType) {
+    case SAVINGS:
+        if (amount <= 1000)
+            return amount * 0.001;
+        else
+            return 1 + (amount - 1000) * 0.002;
+    case SUPER_SAVINGS:
+        if (amount <= 1000)
+            return amount * 0.04; // 2% + 2%
+        if (amount <= 2000)
+            return 40 + (amount - 1000) * 0.07; // 5% + 2%
+        return 140 + (amount - 2000) * 0.12; // 10% + 2%
+    case MAXI_SAVINGS:
+        if (amount <= 1000)
+            return amount * 0.02;
+        if (amount <= 2000)
+            return 20 + (amount - 1000) * 0.05;
+        return 70 + (amount - 2000) * 0.1;
+    default:
+        return amount * 0.001;
         }
     }
 
