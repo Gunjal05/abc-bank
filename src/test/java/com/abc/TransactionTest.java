22badbb3a34488f14abfcccdb10efee7760300d1
Gunjal05 <gunjaljain1997@gmail.com>
2024-10-08 09:46:32 +0530

    Update TransactionTest.java

--- src/test/java/com/abc/TransactionTest.java
+++ src/test/java/com/abc/TransactionTest.java
@@ -3,11 +3,27 @@ package com.abc;
 import org.junit.Test;
 
 import static org.junit.Assert.assertTrue;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+import static org.mockito.Mockito.*;
+import java.util.Date;
 
 public class TransactionTest {
+
+    private DateProvider dateProviderMock;
+
+    @Before
+    public void setUp() {
+        dateProviderMock = mock(DateProvider.class);
+        DateProvider.setInstance(dateProviderMock);
+    }
     @Test
-    public void transaction() {
+    public void testTransactionDate() {
+        Date fixedDate = new Date();
+        when(dateProviderMock.now()).thenReturn(fixedDate);
+
         Transaction t = new Transaction(5);
-        assertTrue(t instanceof Transaction);
+        assertEquals(fixedDate, t.getTransactionDate());
     }
 }
